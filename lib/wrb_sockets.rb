7cdb56bc1e33b9f2e70ff43af6e1bb84dc20d7d3
Cesar Carruitero <cesar@mozilla.pe>
2014-04-09 12:00:51 -0500

    simple websockets implementation

---
+++ lib/wrb_sockets.rb
@@ -0,0 +1,79 @@
+require 'socket'
+require 'websocket/driver'
+
+module WrbSockets
+
+  RECV_SIZE = 1024
+
+  HOST = 'localhost'
+
+  def initialize(port = nil)
+    @server = start_server port
+  end
+
+  def start_server port
+    Socket::TCPServer.open(HOST, port || 0) # maybe can remove first word
+  end
+
+  def port
+    server.addr[1]
+  end
+
+  def handle(socket)
+    env = ENV['RAILS_ENV']
+    #req = Rack::Request.new(env)
+    # check connection is secure and redirect to ssl
+    # secure = req.ssl?
+    # @scheme = secure ? 'wss:' : 'ws:'
+
+    @scheme = 'wss:'
+    #puts req.host_with_port
+    # @url = @scheme + '//' + req.host_with_port #env['HTTP_HOST'] + env['REQUEST_URI']
+    @driver = WebSocket::Driver.server(socket)
+    @clients = []
+
+    @driver.on(:connect) do
+    # in README says connect but in code only exists open
+      @driver.start
+      @clients << @driver
+    end
+
+    @driver.on(:message) do
+      @driver.text(e.data)
+    end
+
+    @driver.on(:close) do
+      puts 'connection with socket are closed'
+    end
+
+    loop do
+      IO.select([socket], [], [], 30) or raise Errno::EWOULDBLOCK
+      data = socket.recv(RECV_SIZE)
+      break if data.empty?
+      driver.parse data
+    end
+  end
+
+  def listen
+    loop do
+      client = server.accept
+      puts "Accepted connection from #{client.addr[2]}"
+      Thread.new { handle client }
+    end
+  end
+
+  def force_ssl
+  end
+
+  def receive_data(data)
+    @driver.parse(data)
+  end
+
+  def write(data)
+    @driver.write(data)
+  end
+end
+
+WrbSocketServer = WrbSockets.new
+# WrbSocketServer::HOST = 'mydomain.something'
+WrbSocketServer.listen
