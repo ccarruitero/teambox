b82ac634c3ed27f9fda18bc0b573304535c85189
ccarruitero <cesar@mozilla.pe>
2014-11-29 09:48:04 -0500

    render tasks

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -18,7 +18,11 @@ Kanban.IndexController = Ember.ArrayController.extend({
 });
 
 Kanban.Router.map(function() {
-  this.resource('task_lists', { path: 'projects/:project_id/task_lists'});
+  this.resource('task_lists',
+                { path: 'projects/:project_id/task_lists'}, function() {
+                  this.resource('task_list', {path: ':task_list_id'});
+                });
+  this.resource('tasks', { path: 'projects/:project_id/tasks'});
 });
 
 Kanban.IndexRoute = Ember.Route.extend({
@@ -42,8 +46,6 @@ Kanban.TaskList = Ember.Model.extend({
   id: attr(),
   name: attr(),
   //status: DS.attr('string'),
-  //assignedId: attr(Number),
-  //dueOn: attr(Date),
   project: belongsTo('Kanban.Project', {key: 'project_id'})
 });
 
@@ -53,6 +55,7 @@ Kanban.TaskList.adapter = Ember.RESTAdapter.create({
     var newURL = url.replace('projectId', parentId);
     return newURL;
   },
+
   findQuery: function(klass, records, params) {
     var self = this;
     var url = self.customURL(klass, params.projectId.project_id);
@@ -63,5 +66,44 @@ Kanban.TaskList.adapter = Ember.RESTAdapter.create({
     });
   }
 });
+
 Kanban.TaskList.url = "api/1/projects/projectId/task_lists";
 Kanban.TaskList.collectionKey = "objects";
+
+Kanban.TasksRoute = Ember.Route.extend({
+  model: function(project_id) {
+    return Kanban.Task.findQuery({projectId: project_id});
+  }
+});
+
+Kanban.Task = Ember.Model.extend({
+  id: attr(),
+  name: attr(),
+  status: attr(),
+  dueOn: attr(Date),
+  assignedId: attr(),
+  completedAt: attr(Date),
+  project: belongsTo('Kanban.Project', {key: 'project_id'}),
+  taskList: belongsTo('Kanban.TaskList', {key: 'task_list_id'}),
+  user: belongsTo('Kanban.User', {key: 'user_id'})
+});
+
+Kanban.Task.adapter = Ember.RESTAdapter.create({
+  customURL: function(klass, parentId) {
+    var url = get(klass, 'url');
+    var newURL = url.replace('projectId', parentId);
+    return newURL;
+  },
+
+  findQuery: function(klass, records, params) {
+    var self = this;
+    var url = self.customURL(klass, params.projectId.project_id);
+
+    return this.ajax(url).then(function(data) {
+      self.didFindAll(klass, records, data);
+      return records;
+    });
+  }
+});
+Kanban.Task.url = "api/1/projects/projectId/tasks";
+Kanban.Task.collectionKey = "objects";
