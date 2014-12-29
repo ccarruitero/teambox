1c0be65ed495a5c6d4d0066db415387e26138f5c
ccarruitero <cesar@mozilla.pe>
2014-12-29 10:41:26 -0500

    move statusName to model

--- app/assets/javascripts/kanban/components/task_detail_component.js
+++ app/assets/javascripts/kanban/components/task_detail_component.js
@@ -2,34 +2,8 @@ Kanban.TaskDetailComponent = Ember.Component.extend({
   tagName: 'section',
   classNames: ['task'],
   classNameBindings: ['statusName'],
-  statusName: '',
   attributeBindings: ['draggable'],
   draggable: 'true',
-
-  init: function() {
-    this._super();
-    var taskStatus = this.get('controller.task')._data.status;
-    this.statusName = this.parseStatusName(taskStatus);
-  },
-
-  parseStatusName: function(status){
-    if (status == 0) {
-    // new
-      return 'backlog';
-    } else if (status == 1) {
-    // open
-      return 'todo';
-    } else if (status == 2) {
-    // hold
-      return 'doing';
-    } else if (status == 3) {
-    // resolved
-      return 'done';
-    } else if (status == 4) {
-    // rejected
-      return 'archived';
-    }
-  },
 
   dragStart: function(e) {
     e.target.style.opacity = '0.7';
--- app/assets/javascripts/kanban/models/task.js
+++ app/assets/javascripts/kanban/models/task.js
@@ -7,7 +7,30 @@ Kanban.Task = Ember.Model.extend({
   completedAt: attr(Date),
   project: belongsTo('Kanban.Project', {key: 'project_id'}),
   taskList: belongsTo('Kanban.TaskList', {key: 'task_list_id'}),
-  user: belongsTo('Kanban.User', {key: 'user_id'})
+  user: belongsTo('Kanban.User', {key: 'user_id'}),
+
+  parseStatusName: function(status){
+    if (status == 0) {
+    // new
+      return 'backlog';
+    } else if (status == 1) {
+    // open
+      return 'todo';
+    } else if (status == 2) {
+    // hold
+      return 'doing';
+    } else if (status == 3) {
+    // resolved
+      return 'done';
+    } else if (status == 4) {
+    // rejected
+      return 'archived';
+    }
+  },
+
+  statusName: function() {
+    return this.parseStatusName(this.get('status'));
+  }.property('status')
 });
 
 Kanban.Task.adapter = Ember.RESTAdapter.create({
