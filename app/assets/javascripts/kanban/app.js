df5fdd284b4cce2eb0712e3687ed010a8ae14a2e
ccarruitero <cesar@mozilla.pe>
2015-01-08 09:36:41 -0500

    setup websocket client

--- app/assets/javascripts/kanban/app.js
+++ app/assets/javascripts/kanban/app.js
@@ -3,6 +3,29 @@ var hasMany = Ember.hasMany;
 var belongsTo = Ember.belongsTo;
 var get = Ember.get;
 
+Ember.Application.initializer({
+  name: 'websocket',
+
+  initialize: function(container, application) {
+    application.deferReadiness()
+    var socketURL = 'ws://' + window.location.host;
+    var socketClient = new WebSocket(socketURL);
+
+    socketClient.onopen = function () {
+      var socket = {
+        client: socketClient
+      };
+
+      application.set('Socket', socket);
+      application.register('socket:main', socket, {instantiate: false});
+      application.inject('route','socket', 'socket:main');
+      application.inject('controller','socket', 'socket:main');
+
+      application.advanceReadiness();
+    };
+  }
+});
+
 window.Kanban = Ember.Application.create({
   rootElement: '#board',
   LOG_MODULE_RESOLVER: true,
